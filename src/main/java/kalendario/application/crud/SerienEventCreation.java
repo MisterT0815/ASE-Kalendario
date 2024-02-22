db3b38fc372746e81abd25f40e8d5740cde131d0
MisterT0815 <larsdebbrecht@gmail.com>
2024-02-22 11:39:12 +0100

    SerienEventCreationhinzugefuegt

---
+++ src/main/java/kalendario/application/crud/SerienEventCreation.java
@@ -0,0 +1,45 @@
+package kalendario.application.crud;
+
+import kalendario.domain.entities.event.*;
+import kalendario.domain.entities.herkunft.Herkunft;
+import kalendario.domain.entities.serie.SerienId;
+import kalendario.domain.repositories.EventRepository;
+import kalendario.domain.repositories.SaveException;
+import kalendario.domain.value_objects.Zeitraum;
+
+import java.util.Date;
+
+public class SerienEventCreation {
+
+    EventRepository eventRepository;
+
+    public SerienEventCreation(EventRepository eventRepository){
+        this.eventRepository = eventRepository;
+    }
+    public Event createEvent(String titel, Herkunft herkunft, Sichtbarkeit sichtbarkeit, String beschreibung, Zeitraum zeitraum, SerienId serie) throws SaveException {
+        EventId id = eventRepository.neueId();
+        Termin termin = new Termin(id, titel, herkunft, sichtbarkeit, beschreibung, serie, zeitraum);
+        eventRepository.saveTermin(termin);
+        return termin;
+    }
+
+    public Event createEvent(String titel, Herkunft herkunft, Sichtbarkeit sichtbarkeit, String beschreibung, Date deadline, boolean getan, SerienId serie) throws SaveException {
+        EventId id = eventRepository.neueId();
+        Aufgabe aufgabe = new Aufgabe(id, titel, herkunft, sichtbarkeit, beschreibung, serie, deadline);
+        if (getan) {
+            aufgabe.setGetan(herkunft.getBesitzerId(), true);
+        }
+        eventRepository.saveAufgabe(aufgabe);
+        return aufgabe;
+    }
+
+    public Event createEvent(String titel, Herkunft herkunft, Sichtbarkeit sichtbarkeit, String beschreibung, Zeitraum zeitraum, boolean getan, SerienId serie) throws SaveException {
+        EventId id = eventRepository.neueId();
+        GeplanteAufgabe geplanteAufgabe = new GeplanteAufgabe(id, titel, herkunft, sichtbarkeit, beschreibung, serie, zeitraum);
+        if (getan) {
+            geplanteAufgabe.setGetan(herkunft.getBesitzerId(), true);
+        }
+        eventRepository.saveGeplanteAufgabe(geplanteAufgabe);
+        return geplanteAufgabe;
+    }
+}
---
+++ src/test/java/kalendario/application/crud/SerienEventCreationTest.java
@@ -0,0 +1,87 @@
+package kalendario.application.crud;
+
+import kalendario.domain.entities.benutzer.BenutzerId;
+import kalendario.domain.entities.event.*;
+import kalendario.domain.entities.herkunft.Herkunft;
+import kalendario.domain.entities.serie.SerienId;
+import kalendario.domain.repositories.EventRepository;
+import kalendario.domain.repositories.SaveException;
+import kalendario.domain.value_objects.Zeitraum;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+
+public class SerienEventCreationTest {
+
+
+    EventId id = mock();
+    String titel = "Titel";
+    Herkunft herkunft = mock();
+    Sichtbarkeit sichtbarkeit = mock();
+    String beschreibung = "Beschreibung";
+    EventRepository eventRepository = mock();
+    SerienId serie = mock();
+    SerienEventCreation serienEventCreation;
+
+    @BeforeEach
+    void init() {
+        serienEventCreation = new SerienEventCreation(eventRepository);
+    }
+
+    @Test
+    void createEventSollTerminSpeichernWennZeitraumMitgegeben() throws SaveException {
+        Zeitraum zeitraum = mock();
+        Event event = serienEventCreation.createEvent(titel, herkunft, sichtbarkeit, beschreibung, zeitraum, serie);
+        assertInstanceOf(Termin.class, event);
+        verify(eventRepository).saveTermin((Termin) event);
+    }
+
+    @Test
+    void createEventSollAufgabeSpeichernWennDeadlineUndGetanMitgegeben() throws SaveException {
+        Date deadline = mock();
+        Event event = serienEventCreation.createEvent(titel, herkunft, sichtbarkeit, beschreibung, deadline,false, serie);
+        assertInstanceOf(Aufgabe.class, event);
+        verify(eventRepository).saveAufgabe((Aufgabe) event);
+    }
+
+    @Test
+    void createEventSollGetanVonAufBesitzerSpeichernWennGetanWahrMitgegebenBeiAufgabe() throws SaveException {
+        Date deadline = mock();
+        BenutzerId besitzerId = mock();
+        when(herkunft.getBesitzerId()).thenReturn(besitzerId);
+        Event event = serienEventCreation.createEvent(titel, herkunft, sichtbarkeit, beschreibung, deadline, true, serie);
+        assertInstanceOf(Aufgabe.class, event);
+        Aufgabe aufgabe = (Aufgabe) event;
+        assertTrue(aufgabe.istGetan());
+        assertEquals(besitzerId, aufgabe.wurdeGemachtVon().get());
+        verify(eventRepository).saveAufgabe(aufgabe);
+    }
+
+    @Test
+    void createEventSollGeplanteAufgabeSpeichernWennZeitraumUndGeplantMitgegeben() throws SaveException {
+        Zeitraum zeitraum = mock();
+        Event event = serienEventCreation.createEvent(titel, herkunft, sichtbarkeit, beschreibung, zeitraum, false, serie);
+        assertInstanceOf(GeplanteAufgabe.class, event);
+        verify(eventRepository).saveGeplanteAufgabe((GeplanteAufgabe) event);
+    }
+
+    @Test
+    void createEventSollGetanVonAufBesitzerSpeichernWennGetanWahrMitgegebenBeiGeplanteAufgabe() throws SaveException {
+        Zeitraum zeitraum = mock();
+        BenutzerId besitzerId = mock();
+        when(herkunft.getBesitzerId()).thenReturn(besitzerId);
+        Event event = serienEventCreation.createEvent(titel, herkunft, sichtbarkeit, beschreibung, zeitraum, true, serie);
+        assertInstanceOf(GeplanteAufgabe.class, event);
+        GeplanteAufgabe geplanteAufgabe = (GeplanteAufgabe) event;
+        assertTrue(geplanteAufgabe.istGetan());
+        assertEquals(besitzerId, geplanteAufgabe.wurdeGemachtVon().get());
+        verify(eventRepository).saveGeplanteAufgabe(geplanteAufgabe);
+    }
+
+}
